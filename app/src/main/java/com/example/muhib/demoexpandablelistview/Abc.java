ab7b13e0e683e95ab4e32f0d0cb868e25dbbf98f
muhib <shah.muhib@gmail.com>
2019-01-02 23:29:15 +0530

    Added

--- app/src/main/java/com/example/muhib/demoexpandablelistview/Abc.java
+++ app/src/main/java/com/example/muhib/demoexpandablelistview/Abc.java
@@ -1,4 +1,9 @@
 package com.example.muhib.demoexpandablelistview;
 
 public class Abc {
+    @androidx.annotation.NonNull
+    @Override
+    public String toString() {
+        return super.toString();
+    }
 }
