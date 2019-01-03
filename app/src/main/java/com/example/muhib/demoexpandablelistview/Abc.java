fece9ca7cf6d70aa549800bc1fc29c407df5e314
muhib <shah.muhib@gmail.com>
2019-01-03 17:32:59 +0530

    Removed toString

--- app/src/main/java/com/example/muhib/demoexpandablelistview/Abc.java
+++ app/src/main/java/com/example/muhib/demoexpandablelistview/Abc.java
@@ -1,11 +1,6 @@
 package com.example.muhib.demoexpandablelistview;
 
 public class Abc {
-    @androidx.annotation.NonNull
-    @Override
-    public String toString() {
-        return super.toString();
-    }
 
     @Override
     public boolean equals(@androidx.annotation.Nullable Object obj) {
